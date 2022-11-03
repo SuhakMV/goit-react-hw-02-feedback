4a7ffd3faf2e41d4e6012e89484fc795785aadf3
SuhakMV <sugamish@gmail.com>
2022-11-03 17:50:28 +0200

    03111750

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './Fedback/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { NotificationMessage } from './Statistics/NotificationMessage';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({
+      [state]: prevState[state] + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return this.countTotalFeedback() === 0
+      ? 0
+      : Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <div className="section">
+          <Section title={'Please leave feedback'}>
+            <FeedbackOptions
+              options={options}
+              onLeaveFeedback={this.onLeaveFeedback}
+            />
+          </Section>
+
+          <Section title={'Statistics'}>
+            {this.countTotalFeedback() > 0 ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={this.countTotalFeedback()}
+                positivePercentage={this.countPositiveFeedbackPercentage()}
+              />
+            ) : (
+              <NotificationMessage message="There is no feedback" />
+            )}
+          </Section>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
